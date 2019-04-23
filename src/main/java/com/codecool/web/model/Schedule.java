399b4d5de162006526484968a8712370a0a6adb8
Szűcs Áron <aron.szucs.miskolc@gmail.com>
2019-04-23 12:49:08 +0200

    skeleton code for model, service and dao classes created

---
+++ src/main/java/com/codecool/web/model/Schedule.java
@@ -0,0 +1,13 @@
+package com.codecool.web.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Schedule {
+
+    public void addTast(Task task) {
+    }
+
+    public void getTasks() {
+    }
+}
