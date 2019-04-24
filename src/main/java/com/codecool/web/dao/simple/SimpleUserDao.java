b8eadd82122016bd6e8a69b079728b5264591813
Gergő Papp <imgergopapp@gmail.com>
2019-04-24 10:55:27 +0200

    Added skeleton code for DAO method implementations

--- src/main/java/com/codecool/web/dao/simple/SimpleUserDao.java
+++ src/main/java/com/codecool/web/dao/simple/SimpleUserDao.java
@@ -2,49 +2,50 @@ package com.codecool.web.dao.simple;
 
 import com.codecool.web.dao.AbstractDao;
 import com.codecool.web.dao.UserDao;
+import com.codecool.web.model.User;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class SimpleUserDao extends AbstractDao implements UserDao  {
+public class SimpleUserDao extends AbstractDao implements UserDao {
+
     public SimpleUserDao(Connection connection) {
         super(connection);
     }
 
-    public DatabaseUserDao(Connection connection) {
-        super(connection);
+    private User fetchUser(ResultSet resultSet) throws SQLException {
+        return null;
     }
 
     @Override
     public List<User> findAll() throws SQLException {
-
+        return null;
     }
 
     @Override
     public User findById(int id) throws SQLException {
-
+        return null;
     }
 
     @Override
     public User add(String name, int percentage) throws SQLException {
-
+        return null;
     }
 
+    @Override
     public void add(int userId, int... scheduleIds) throws SQLException {
 
     }
 
+    @Override
     public void updateUser(String name) throws SQLException {
 
     }
 
+    @Override
     public void deleteUser(String name) throws SQLException {
-
-    }
-
-    private User fetchUser(ResultSet resultSet) throws SQLException {
 
     }
 }
