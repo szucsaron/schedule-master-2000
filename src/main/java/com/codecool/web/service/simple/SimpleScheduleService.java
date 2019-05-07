88dae79d520dff48dd2edaa1e056cc0b45b10e62
Gergő Papp <imgergopapp@gmail.com>
2019-05-07 10:10:29 +0200

    Implemented ScheduleService interface's methods

--- src/main/java/com/codecool/web/service/simple/SimpleScheduleService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleScheduleService.java
@@ -2,8 +2,13 @@ package com.codecool.web.service.simple;
 
 
 import com.codecool.web.dao.ScheduleDao;
+import com.codecool.web.model.Schedule;
+import com.codecool.web.service.ScheduleService;
 
-public class SimpleScheduleService {
+import java.sql.SQLException;
+import java.util.List;
+
+public class SimpleScheduleService implements ScheduleService {
 
     private final ScheduleDao scheduleDao;
 
@@ -12,4 +17,34 @@ public class SimpleScheduleService {
     }
 
 
+    @Override
+    public List<Schedule> findAll() throws SQLException {
+        return scheduleDao.findAll();
+    }
+
+    @Override
+    public List<Schedule> findByUser(String userId) throws SQLException {
+        return scheduleDao.findByUser(Integer.parseInt(userId));
+    }
+
+    @Override
+    public Schedule findById(String scheduleId, String userId) throws SQLException {
+        return scheduleDao.findById(Integer.parseInt(scheduleId), Integer.parseInt(userId));
+    }
+
+    @Override
+    public void add(String userId, String name) throws SQLException {
+        scheduleDao.add(Integer.parseInt(userId), name);
+    }
+
+    @Override
+    public void update(String scheduleId, String userId, String name) throws SQLException {
+        scheduleDao.update(Integer.parseInt(userId), Integer.parseInt(scheduleId), name);
+    }
+
+    @Override
+    public void delete(String scheduleId, String userId) throws SQLException {
+        scheduleDao.delete(Integer.parseInt(scheduleId), Integer.parseInt(userId));
+
+    }
 }
