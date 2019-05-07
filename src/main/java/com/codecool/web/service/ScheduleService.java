d98ffcad3a0842d80562d275cc6c027f6e19524e
Gergő Papp <imgergopapp@gmail.com>
2019-05-07 10:08:38 +0200

    Modified ScheduleService interface

--- src/main/java/com/codecool/web/service/ScheduleService.java
+++ src/main/java/com/codecool/web/service/ScheduleService.java
@@ -1,8 +1,6 @@
 package com.codecool.web.service;
 
 import com.codecool.web.model.Schedule;
-import com.codecool.web.model.Task;
-import com.codecool.web.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -11,16 +9,14 @@ public interface ScheduleService {
 
     List<Schedule> findAll() throws SQLException;
 
-    List<Schedule> findByUser(User user) throws SQLException;
+    List<Schedule> findByUser(String userId) throws SQLException;
 
-    Schedule findEmptyById(int id) throws SQLException;
+    Schedule findById(String scheduleId, String userId) throws SQLException;
 
-    Schedule findFilledById(int id) throws SQLException;
+    void add(String userId, String name) throws SQLException;
 
-    void add(Task task) throws SQLException;
+    void update(String scheduleId, String userId, String name) throws SQLException;
 
-    void update(Task task) throws SQLException;
-
-    void delete(int id) throws SQLException;
+    void delete(String scheduleId, String userId) throws SQLException;
 
 }
