a1ae19ed563a49cee2d9e866211a7a494974e796
szucsaron <aron.szucs.miskolc@gmail.com>
2019-05-07 09:33:24 +0200

    TaskService created

--- src/main/java/com/codecool/web/service/TaskService.java
+++ src/main/java/com/codecool/web/service/TaskService.java
@@ -1,45 +1,81 @@
-/*package com.codecool.web.service;
+package com.codecool.web.service;
+
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.model.Schedule;
+import com.codecool.web.model.Task;
+import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class TaskService {
 
-    public List<Task> getTasks() throws SQLException {
+    private TaskDao taskDao;
+
+    public TaskService(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+
+    public List<Task> findAll() throws SQLException, ServiceException {
         return taskDao.findAll();
     }
 
-    @Override
-    public Task getTask(String id) throws SQLException, ServiceException {
-     
+    public List<Task> findByScheduleId(String scheduleId) throws SQLException, ServiceException {
+        try {
+            return taskDao.findByScheduleId(Integer.parseInt(scheduleId));
+        } catch (NumberFormatException e) {
+            throw new ServiceException("shceduleId must be an integer");
+        }
     }
 
-    @Override
-    public Task addTask(String name, String percentage) throws SQLException, ServiceException {
-      
+    public Task findById(String id) throws SQLException, ServiceException {
+        try {
+            return taskDao.findById(Integer.parseInt(id));
+        } catch (NumberFormatException e) {
+            throw new ServiceException("id must be an integer");
+        }
     }
 
-    public void updateTask(String name) throws SQLException, ServiceException {
-
+    public int add(String title, String content) throws SQLException {
+        return taskDao.add(title, content);
     }
 
-    public void deleteTask(String name) throws SQLException, ServiceException {
-
+    public void update(String id, String title, String content) throws SQLException, ServiceException {
+        try {
+            taskDao.update(Integer.parseInt(id), title, content);
+        } catch (NumberFormatException e) {
+            throw new ServiceException("id must be an integer");
+        }
     }
 
-    @Override
-    public void addTaskToSchedules(String taskId, String... scheduleIds) throws SQLException, ServiceException {
-      
+    public void addToSchedule(String scheduleId, String taskId, String date, String hourStart, String hourEnd) throws SQLException, ServiceException {
+        int scheduleIdVal;
+        int taskIdVal;
+        int hourStartVal;
+        int hourEndVal;
+        LocalDate dateVal;
+        try {
+            scheduleIdVal = Integer.parseInt(scheduleId);
+        } catch (NumberFormatException e) {
+            throw new ServiceException("scheduleId must be an integer");
+        }
+        try {
+            taskIdVal = Integer.parseInt(taskId);
+        } catch (NumberFormatException e) {
+            throw new ServiceException("taskIdVal must be an integer");
+        }
+        try {
+            hourStartVal = Integer.parseInt(hourStart);
+        } catch (NumberFormatException e) {
+            throw new ServiceException("hourStartVal must be an integer");
+        }
+        try {
+            hourEndVal = Integer.parseInt(hourEnd);
+        } catch (NumberFormatException e) {
+            throw new ServiceException("hourEndVal must be an integer");
+        }
+        dateVal = LocalDate.parse(date);
+        taskDao.addToSchedule(scheduleIdVal, taskIdVal, dateVal, hourStartVal, hourEndVal);
     }
-
-    @Override
-    public List<Schedule> getTaskSchedules(String taskId) throws SQLException, ServiceException {
- 
-    }
-
-    private int[] parseScheduleIds(String[] scheduleIds) throws ServiceException {
-   
-    }
-
-
-}*/
+}
