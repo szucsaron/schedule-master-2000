7f1b516bed927decc65f2348081d57b1f372d61f
Gergő Papp <imgergopapp@gmail.com>
2019-05-08 09:20:52 +0200

    Implemented Constructor (overload)

--- src/main/java/com/codecool/web/model/Schedule.java
+++ src/main/java/com/codecool/web/model/Schedule.java
@@ -1,15 +1,30 @@
 package com.codecool.web.model;
 
+import java.time.LocalDate;
+
 public class Schedule {
 
     private int id;
     private int userId;
     private String name;
+    private LocalDate startingDate;
+    private int durationInDays;
 
+
+    // TODO : Remove old constructor (testing only)
     public Schedule(int id, int userId, String name) {
         this.id = id;
         this.userId = userId;
         this.name = name;
+
+    }
+
+    public Schedule(int id, int userId, String name, LocalDate startingDate, int durationInDays) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.startingDate = startingDate;
+        this.durationInDays = durationInDays;
     }
 
     /*public void addTast(Task task) {
