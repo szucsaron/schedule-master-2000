cd6906ca12793941824a616a1a613fa8c0832a60
Gergő Papp <imgergopapp@gmail.com>
2019-05-08 09:20:00 +0200

    Implemented HTTP methods

--- src/main/java/com/codecool/web/servlet/SchedulesServlet.java
+++ src/main/java/com/codecool/web/servlet/SchedulesServlet.java
@@ -1,4 +1,105 @@
 package com.codecool.web.servlet;
 
-public class SchedulesServlet {
+import com.codecool.web.dao.ScheduleDao;
+import com.codecool.web.dao.simple.SimpleScheduleDao;
+import com.codecool.web.model.Schedule;
+import com.codecool.web.model.User;
+import com.codecool.web.service.ScheduleService;
+import com.codecool.web.service.simple.SimpleScheduleService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+@WebServlet("/schedules")
+public class SchedulesServlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // List ALL Schedules
+        // TODO : list only by userId
+
+        try (Connection connection = getConnection(req.getServletContext())) {
+            ScheduleDao scheduleDao = new SimpleScheduleDao(connection);
+            ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
+
+            User loggedInUser = (User) req.getSession().getAttribute("user");
+            String userId = String.valueOf(loggedInUser.getId());
+            scheduleService.add(userId, "schedule name here");
+
+            List<Schedule> schedules = scheduleService.findAll();
+
+            sendMessage(resp, SC_OK, schedules);
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Create Schedule
+
+        try (Connection connection = getConnection(req.getServletContext())) {
+            ScheduleDao scheduleDao = new SimpleScheduleDao(connection);
+            ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
+
+            User loggedInUser = (User) req.getSession().getAttribute("user");
+            String userId = String.valueOf(loggedInUser.getId());
+            scheduleService.add(userId, "schedule name here");
+
+            sendMessage(resp, SC_OK, "new schedule created");
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Modify Schedule (RENAME ONLY)
+        // TODO : need to get the schedule's ID
+
+        try (Connection connection = getConnection(req.getServletContext())) {
+            ScheduleDao scheduleDao = new SimpleScheduleDao(connection);
+            ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
+
+            User loggedInUser = (User) req.getSession().getAttribute("user");
+            String userId = String.valueOf(loggedInUser.getId());
+            scheduleService.update("1", userId, "updated schedule name here");
+
+            sendMessage(resp, SC_OK, "Schedule's name modified");
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Delete Schedule
+        // TODO : need to get the schedule's ID
+
+        try (Connection connection = getConnection(req.getServletContext())) {
+            ScheduleDao scheduleDao = new SimpleScheduleDao(connection);
+            ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
+
+            User loggedInUser = (User) req.getSession().getAttribute("user");
+            String userId = String.valueOf(loggedInUser.getId());
+            scheduleService.delete("1", userId);
+
+            sendMessage(resp, SC_OK, "Schedule deleted");
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+    }
 }
