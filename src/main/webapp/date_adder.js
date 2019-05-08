7b558645c496b4cee41f7f9773666be66059bfbb
szucsaron <aron.szucs.miskolc@gmail.com>
2019-05-08 15:49:47 +0200

    unneeded function removed from date_adder

--- src/main/webapp/date_adder.js
+++ src/main/webapp/date_adder.js
@@ -8,13 +8,3 @@ function convertDate(localDate) {
     const str = localDate.year + "-" + localDate.monthValue + "-" + localDate.dayOfMonth;
     return new Date(str);
 }
-
-function getDateValues(date) {
-    const str = date.toJSON();
-    const year = str.substr(0, 4);
-    const month = str.substr(5, 6);
-    const day = str.substr(8, 9);
-
-    alert(month);
-
-}
