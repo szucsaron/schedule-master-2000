c9fbcb7c10b2b46ee636b5744a0e996edc9794c1
szucsaron <aron.szucs.miskolc@gmail.com>
2019-05-20 11:50:30 +0200

    appendTask moved to tasks.js

--- src/main/webapp/tasks.js
+++ src/main/webapp/tasks.js
@@ -10,6 +10,33 @@ function appendTasks(tasks) {
     }
 }
 
+function appendTask(task) {
+    /*const idTdEl = document.createElement('td');
+    idTdEl.textContent = task.id;*/
+
+    const aEl = document.createElement('a');
+    aEl.textContent = task.title;
+    aEl.href = 'javascript:void(0);';
+    aEl.dataset.taskId = task.id;
+    aEl.addEventListener('click', onTaskClicked);
+
+    const titleTdEl = document.createElement('td');
+    titleTdEl.appendChild(aEl);
+
+    const contentTdEl = document.createElement('td');
+    contentTdEl.textContent = task.content;
+
+    /*const dateTdEl = document.createElement('td');
+    dateTdEl.textContent = task.date.dayOfMonth + "." + task.date.monthValue + "." + task.date.year;*/
+
+    const trEl = document.createElement('tr');
+    // trEl.appendChild(idTdEl);
+    trEl.appendChild(titleTdEl);
+    trEl.appendChild(contentTdEl);
+    //trEl.appendChild(dateTdEl);
+    tasksTableBodyEl.appendChild(trEl);
+}
+
 function onTasksLoad(tasks) {
     tasksTableEl = document.getElementById('tasks');
     tasksTableBodyEl = tasksTableEl.querySelector('tbody');
