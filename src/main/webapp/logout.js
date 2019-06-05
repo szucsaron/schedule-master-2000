505038a762c8ca7844bf92d626902bed45c14335
Gergő Papp <imgergopapp@gmail.com>
2019-06-05 15:14:29 +0200

    Added removeChild on Logger menupoint

--- src/main/webapp/logout.js
+++ src/main/webapp/logout.js
@@ -2,6 +2,7 @@ function onLogoutResponse() {
     if (this.status === OK) {
         if (JSON.parse(localStorage.getItem('user')).role == 'ADMIN') {
             document.getElementById("user-menu-tr").removeChild(adminMenuTdEl);
+            document.getElementById("user-menu-tr").removeChild(logMenuTdEl);
         }
         setUnauthorized();
         clearMessages();
