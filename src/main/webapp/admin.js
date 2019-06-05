f5a47b8c9991384d905ad89ec86fd7cf8f9a8ff0
Gergő Papp <imgergopapp@gmail.com>
2019-06-05 15:11:12 +0200

    Implemented Logger methods

--- src/main/webapp/admin.js
+++ src/main/webapp/admin.js
@@ -1,6 +1,9 @@
 let usersTableEl;
 let usersTableBodyEl;
 
+let logsTableEl
+let logsTableBodyEl;
+
 
 function onAllUsersReceived() {
     if (this.status === OK) {
@@ -66,4 +69,67 @@ function onUserClicked() {
     xhr.addEventListener('error', onNetworkError);
     xhr.open('GET', 'protected/schedules?' + params.toString());
     xhr.send();
+}
+
+function onLoggerClicked() {
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onAllLogsReceived);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', 'protected/logger');
+    xhr.send();
+}
+
+function onAllLogsReceived() {
+    if (this.status === OK) {
+        showContents(['user-menu','logs-content']);
+        onAllLogsLoad(JSON.parse(this.responseText));
+    } else {
+        onOtherResponse(schedulesContentDivEl, this); // needs update 
+    }
+}
+
+function onAllLogsLoad(logs) {
+    logsTableEl = document.getElementById('logs-table');
+    logsTableBodyEl = logsTableEl.querySelector('tbody');
+
+    appendLogsTable(logs);
+}
+
+function appendLogsTable(logs) { 
+    removeAllChildren(logsTableBodyEl);
+
+    for (let i = 0; i < logs.length; i++) {
+        const log = logs[i];
+        appendLogs(log);
+    }
+}
+
+function appendLogs(log) { // 3
+
+    const dateTdEl = document.createElement('td');
+    const date = new Date(log.date);
+    const options = {hour12:false, year: 'numeric', month: 'long', day: 'numeric' , hour: 'numeric', minute: 'numeric', second: 'numeric'};
+    dateTdEl.textContent = date.toLocaleDateString("en-US", options)
+
+    const nameTdEl = document.createElement('td');
+    nameTdEl.textContent = log.username;
+
+    const typeTdEl = document.createElement('td');
+    typeTdEl.textContent = log.type;
+
+    const locationTdEl = document.createElement('td');
+    locationTdEl.textContent = log.location;
+
+    const messageTdEl = document.createElement('td');
+    messageTdEl.textContent = log.msg;
+
+    const trEl = document.createElement('tr');
+
+    trEl.appendChild(dateTdEl);
+    trEl.appendChild(nameTdEl);
+    trEl.appendChild(typeTdEl);
+    trEl.appendChild(locationTdEl);
+    trEl.appendChild(messageTdEl);
+    logsTableBodyEl.appendChild(trEl);
 }
