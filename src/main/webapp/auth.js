86ed2ccf601da3c2de71619362fab2d8a2cc2486
Gergő Papp <imgergopapp@gmail.com>
2019-06-05 15:25:58 +0200

    Changed page refresh to login

--- src/main/webapp/auth.js
+++ src/main/webapp/auth.js
@@ -1,10 +1,12 @@
 const xhr = new XMLHttpRequest();
 xhr.onload = function() {
-    if(this.status !== 200) {
+   /* if(this.status !== 200) {
         showContents(['login-content']);
     } else {
         showContents(['user-menu', 'welcome-content']);
     }
+*/
+    showContents(['login-content']);
 }
 xhr.open('GET', 'protected/auth');
 xhr.send();
