b6b433965ad428106ce32291535c857ec416c342
Gergő Papp <imgergopapp@gmail.com>
2019-06-06 10:01:13 +0200

    Refactored code & added real path for log

--- src/main/java/com/codecool/web/dao/simple/SimpleLoggerDao.java
+++ src/main/java/com/codecool/web/dao/simple/SimpleLoggerDao.java
@@ -6,7 +6,6 @@ import com.codecool.web.model.Log;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-
 import java.sql.Connection;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -22,36 +21,31 @@ public class SimpleLoggerDao extends AbstractDao implements LoggerDao {
         super(connection);
     }
 
-    public List<Log> getLogContent() throws FileNotFoundException, ParseException {
+    public List<Log> getLogContent(String path) throws FileNotFoundException, ParseException {
         List<Log> logs = new ArrayList<>();
         String[] properties;
-        String username ="";
-        String hardcodePath = "/home/pgergo/codecool/WEB/apache-tomcat-8.5.38/logs/schmaster.log";
-        Scanner s = new Scanner(new File(hardcodePath));
-            while (s.hasNextLine()) {
-                String line = s.nextLine();
-                properties = line.split(";");
-                if (properties.length == 7){
-                    username = properties[6];
-                }
-                else if(properties.length==1){
-                    continue;
-                }
-                logs.add(fetchLog(properties, username));
+        String username = "";
+        Scanner s = new Scanner(new File(path + "../../logs/schmaster.log"));
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            properties = line.split(";");
+            if (properties.length == 7) {
+                username = properties[6];
+            } else if (properties.length == 1) {
+                continue;
             }
+            logs.add(fetchLog(properties, username));
+        }
         return logs;
     }
 
-    private Log fetchLog(String[] properties,String username) throws ParseException{
-        String date = properties[0].substring(0,properties[0].length()-4);
+    private Log fetchLog(String[] properties, String username) throws ParseException {
+        String date = properties[0].substring(0, properties[0].length() - 4);
         String type = properties[1];
         String location = properties[4];
         String msg = properties[5];
 
-        if (properties[1].equals("Logged out")){
-        }
-
-        return new Log(username ,dateParser(date),type,location,msg);
+        return new Log(username, dateParser(date), type, location, msg);
     }
 
     private Date dateParser(String stringDate) throws ParseException {
@@ -59,5 +53,4 @@ public class SimpleLoggerDao extends AbstractDao implements LoggerDao {
         Date date = formatter.parse(stringDate);
         return date;
     }
-
 }
